56862422d6d0f2ae39c0c29e29812bd855955413
TaahirBhorat <taahir.bhorat3@gmail.com>
2024-01-17 14:04:20 +0200

    Adding Ntsabo Radar

--- pages/3_Summarise.py
+++ pages/3_Summarise.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Left Back Radar Plots')
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(2)
 with col1:
     st.image('Plots/Dove.png', use_column_width=True)
     st.image('Plots/Fawaaz.png', use_column_width=True)
@@ -10,7 +10,7 @@ with col2:
     st.image('Plots/Ndaba.png', use_column_width=True)
     st.image('Plots/Sibande.png', use_column_width=True)
 
-# Center the last image in a new row
-col3, col4, col5 = st.columns([1,2,1])
-with col4:
+
+with col3:
     st.image('Plots/Sithole.png',  use_column_width=True)
+    st.image('Plots/Ntsabo.png', use_column_width=True)
