21ef839b506726c7813862b39dd8431289a43633
TaahirBhorat <taahir.bhorat3@gmail.com>
2024-01-17 14:05:23 +0200

    fix

--- pages/3_Summarise.py
+++ pages/3_Summarise.py
@@ -2,14 +2,13 @@ import streamlit as st
 
 st.title('Left Back Radar Plots')
 
-col1, col2, col3 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 with col1:
     st.image('Plots/Dove.png', use_column_width=True)
     st.image('Plots/Fawaaz.png', use_column_width=True)
 with col2:
     st.image('Plots/Ndaba.png', use_column_width=True)
     st.image('Plots/Sibande.png', use_column_width=True)
-
 
 with col3:
     st.image('Plots/Sithole.png',  use_column_width=True)
